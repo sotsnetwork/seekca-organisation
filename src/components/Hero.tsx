1d5823ae385e318561608400d35c60ae3bddcbcd
Chidera Nwachukwu <chideranwachukwu32@gmail.com>
2025-09-08 16:18:52 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -82,7 +82,7 @@ export default function Hero() {
                   <span className="block bg-gradient-hero bg-clip-text text-transparent">
                     Verified Pros
                   </span>
-                  Nearby
+                  Near You
                 </>
               )}
             </h1>
